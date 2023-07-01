213312963ef6b07312a32b747ce61a5d7e0c8d79
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-06-30 23:15:15 -0400

    adds tests for github service

---
+++ src/lib/constants/github.ts
@@ -0,0 +1,16 @@
+export const GitHub = {
+  Labels: {
+    Article: 'edu:article',
+    Book: 'edu:book',
+    Course: 'edu:course',
+    Exclude: 'exclude',
+    Video: 'edu:video',
+    Suggested: 'suggested-reading',
+  },
+  Repos: {
+    TheShop: 'the-shop',
+  },
+  User: {
+    Username: 'iamthe-Wraith',
+  },
+} as const;
--- src/lib/services/github.ts
+++ src/lib/services/github.ts
@@ -1,4 +1,5 @@
 import { GITHUB_API_TOKEN } from '$env/static/private';
+import { GitHub } from '$lib/constants/github';
 import { Octokit } from '@octokit/core';
 
 /**
@@ -37,40 +38,44 @@ interface IBaseGHIssue {
   state: string;
 }
 
-interface IGHIssue extends IBaseGHIssue {
+export interface IGHIssue extends IBaseGHIssue {
   body: string;
 }
 
-interface IGHArticleIssue extends IBaseGHIssue {
+export interface IGHArticleIssue extends IBaseGHIssue {
   body: IArticle;
 }
 
-interface IGHBookIssue extends IBaseGHIssue {
+export interface IGHBookIssue extends IBaseGHIssue {
   body: IBook;
 }
 
-interface IGHCourseIssue extends IBaseGHIssue {
+export interface IGHCourseIssue extends IBaseGHIssue {
   body: ICourse;
 }
 
-const determineIfIsSuggested = (issue: IGHIssue) => {
-  return issue.labels.filter((label) => (label as IGHLabel).name === 'suggested-reading').length > 0;
+export interface IGHVideoIssue extends IBaseGHIssue {
+  body: IVideo;
 }
 
-const getArticles = (learnings: IGHIssue[]): IArticle[] => {
+export const isSuggested = (issue: IGHIssue) => {
+  return issue.labels.filter((label) => (label as IGHLabel).name === GitHub.Labels.Suggested).length > 0;
+}
+
+export const getArticles = (learnings: IGHIssue[]): IArticle[] => {
   const articles = learnings.filter((issue) => {
-    return issue.labels.filter((label) => (label as IGHLabel).name === 'edu:article').length;
+    return issue.labels.filter((label) => (label as IGHLabel).name === GitHub.Labels.Article).length;
   })
     .map((issue) => {
       const { url, description } = (issue as unknown as IGHArticleIssue).body;
 
       if (!url || !description) {
-        console.log(`>>>>> invalid article body found for ${issue.title}. Missing url or description`);
+        console.log(`invalid article body found for ${issue.title}. missing url or description`);
         return null;
       } else {
         return {
           ...(issue as unknown as IGHArticleIssue).body,
-          isSuggested: determineIfIsSuggested(issue),
+          isSuggested: isSuggested(issue),
           title: issue.title,
         }
       }
@@ -80,43 +85,43 @@ const getArticles = (learnings: IGHIssue[]): IArticle[] => {
   return articles as IArticle[];
 }
 
-const getBooks = (learnings: IGHIssue[]): IBook[] => {
+export const getBooks = (learnings: IGHIssue[]): IBook[] => {
   const books = learnings.filter((issue) => {
-    return issue.labels.filter((label) => (label as IGHLabel).name === 'edu:book').length;
+    return issue.labels.filter((label) => (label as IGHLabel).name === GitHub.Labels.Book).length;
   })
     .map((issue) => {
       const { author, url, description } = (issue as unknown as IGHBookIssue).body;
 
       if (!author || !url || !description) {
-        console.log(`>>>>> invalid book body found for ${issue.title}. Missing author, url or description`);
+        console.log(`invalid book body found for ${issue.title}. missing author, url or description`);
         return null;
       } else {
         return {
           ...(issue as unknown as IGHBookIssue).body,
-          isSuggested: determineIfIsSuggested(issue),
+          isSuggested: isSuggested(issue),
           title: issue.title,
         }
       }
     })
-    .filter((article) => article !== null);
+    .filter((book) => book !== null);
 
   return books as IBook[];
 }
 
-const getCourses = (learnings: IGHIssue[]): ICourse[] => {
+export const getCourses = (learnings: IGHIssue[]): ICourse[] => {
   const courses = learnings.filter((issue) => {
-    return issue.labels.filter((label) => (label as IGHLabel).name === 'edu:course').length;
+    return issue.labels.filter((label) => (label as IGHLabel).name === GitHub.Labels.Course).length;
   })
     .map((issue) => {
       const { url, description } = (issue as unknown as IGHCourseIssue).body;
 
       if (!url || !description) {
-        console.log(`>>>>> invalid course body found for ${issue.title}. Missing url or description`);
+        console.log(`invalid course body found for ${issue.title}. missing url or description`);
         return null;
       } else {
         return {
           ...(issue as unknown as IGHCourseIssue).body,
-          isSuggested: determineIfIsSuggested(issue),
+          isSuggested: isSuggested(issue),
           title: issue.title,
         }
       }
@@ -126,20 +131,20 @@ const getCourses = (learnings: IGHIssue[]): ICourse[] => {
   return courses as ICourse[];
 }
 
-const getVideos = (learnings: IGHIssue[]): IVideo[] => {
+export const getVideos = (learnings: IGHIssue[]): IVideo[] => {
   const videos = learnings.filter((issue) => {
-    return issue.labels.filter((label) => (label as IGHLabel).name === 'edu:video').length;
+    return issue.labels.filter((label) => (label as IGHLabel).name === GitHub.Labels.Video).length;
   })
     .map((issue) => {
       const { url, description } = (issue as unknown as IGHCourseIssue).body;
 
       if (!url || !description) {
-        console.log(`>>>>> invalid course body found for ${issue.title}. Missing url or description`);
+        console.log(`invalid course body found for ${issue.title}. missing url or description`);
         return null;
       } else {
         return {
           ...(issue as unknown as IGHCourseIssue).body,
-          isSuggested: determineIfIsSuggested(issue),
+          isSuggested: isSuggested(issue),
           title: issue.title,
         }
       }
@@ -149,7 +154,7 @@ const getVideos = (learnings: IGHIssue[]): IVideo[] => {
   return videos as IVideo[];
 }
 
-const parseBody = (issue: IGHIssue): IArticle | IBook | ICourse | null => {
+export const parseIssueBody = (issue: IGHIssue): IArticle | IBook | ICourse | null => {
   let parsed = null;
   let sterilized = '';
 
@@ -161,8 +166,8 @@ const parseBody = (issue: IGHIssue): IArticle | IBook | ICourse | null => {
       throw new Error('No body found');
     }
   } catch (err: any) {
-    console.log('>>>>> error parsing issue body', issue.title);
-    console.log('>>>>> ', err.message);
+    console.log(`error parsing issue body for issue: ${issue.title}`);
+    console.log(err.message);
   }
 
   return parsed;
@@ -171,8 +176,8 @@ const parseBody = (issue: IGHIssue): IArticle | IBook | ICourse | null => {
 const getAllLearnings = async (): Promise<IGHIssue[]> => {
   const response = await octokit.request('GET /repos/{owner}/{repo}/issues{?state}', {
     state: 'closed',
-    owner: 'iamthe-Wraith',
-    repo: 'the-shop',
+    owner: GitHub.User.Username,
+    repo: GitHub.Repos.TheShop,
   });
 
   // pull all issues with suggested-reading label
@@ -181,14 +186,14 @@ const getAllLearnings = async (): Promise<IGHIssue[]> => {
     let isExcluded = false;
 
     for (const label of issue.labels) {
-      if ((label as IGHLabel).name === 'edu:article' ||
-      (label as IGHLabel).name === 'edu:book' ||
-      (label as IGHLabel).name ===  'edu:course' ||
-      (label as IGHLabel).name ===  'edu:video') {
+      if ((label as IGHLabel).name === GitHub.Labels.Article ||
+      (label as IGHLabel).name === GitHub.Labels.Book ||
+      (label as IGHLabel).name ===  GitHub.Labels.Course ||
+      (label as IGHLabel).name ===  GitHub.Labels.Video) {
         isLearning = true;
       }
 
-      if ((label as IGHLabel).name === 'exclude') {
+      if ((label as IGHLabel).name === GitHub.Labels.Exclude) {
         isExcluded = true;
       }
     }
@@ -196,7 +201,7 @@ const getAllLearnings = async (): Promise<IGHIssue[]> => {
     return isLearning && !isExcluded;
   })
     .map((issue: IGHIssue) => {
-      const body = parseBody(issue);
+      const body = parseIssueBody(issue);
       return { ...issue, body };
     })
     .filter((issue: IGHIssue) => issue.body);
