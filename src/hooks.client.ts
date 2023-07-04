168e639ccfbd67ab56ca2a1906c7720997574e24
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-07-04 18:49:33 -0400

    adds initializing Sentry for client and server

---
+++ src/hooks.client.ts
@@ -0,0 +1,18 @@
+import * as Sentry from '@sentry/sveltekit';
+import { PUBLIC_SENTRY_DSN, PUBLIC_APP_ENV } from '$env/static/public';
+import type { HandleClientError } from '@sveltejs/kit';
+
+const errorHandler = (({ error, event }) => {
+  console.error('An error occurred on the client side:', error, event);
+}) satisfies HandleClientError;
+
+Sentry.init({
+  dsn: PUBLIC_SENTRY_DSN,
+  environment: PUBLIC_APP_ENV,
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [new Sentry.Replay()],
+});
+
+export const handleError = Sentry.handleErrorWithSentry(errorHandler);
