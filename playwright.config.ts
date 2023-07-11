c2384f0b65a6c2414b2fd21084c0ae492d0ce43c
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-07-11 19:52:13 -0400

    adds init component test infrastructure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,12 @@
 import { devices, type PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	},
-	testDir: 'tests/e2e',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+  webServer: {
+    command: 'npm run build && npm run preview',
+    port: 4173
+  },
+  testDir: 'tests/e2e',
+  testMatch: /(.+\.)?(test|spec)\.[jt]s/,
   projects: [
     {
       name: 'chromium',
