d44e288adb87c89447070a6c52e878fdeac5c0d3
Jake Lundberg <allhailthewraith@protonmail.com>
2023-07-11 20:01:09 -0400

    test: updates playwright (#28)

---
+++ tests/component/Nav.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/experimental-ct-svelte';
+import Nav from '../../src/lib/components/Nav/Nav.svelte';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('should work', async ({ mount }) => {
+  const component = await mount(Nav);
+  await expect(component).toContainText('Vite + Svelte');
+});
