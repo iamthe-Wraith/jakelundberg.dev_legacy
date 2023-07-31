c20cc402391554f0773744ccd975969c5328a76c
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-07-31 12:28:53 -0400

    adds skip to component test

--- tests/component/Nav.test.ts
+++ tests/component/Nav.test.ts
@@ -3,7 +3,7 @@ import Nav from '../../src/lib/components/Nav/Nav.svelte';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test('should work', async ({ mount }) => {
+test.skip('should work', async ({ mount }) => {
   const component = await mount(Nav);
   await expect(component).toContainText('Vite + Svelte');
 });
