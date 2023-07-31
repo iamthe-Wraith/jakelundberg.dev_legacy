b59b18e42379bd9c86ca792a4bf720145838b3e1
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-07-31 13:01:17 -0400

    adds reporter config to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ const config: PlaywrightTestConfig = {
   },
   testDir: 'tests/e2e',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+  reporter: process.env.CI ? 'github' : 'list',
   projects: [
     {
       name: 'chromium',
