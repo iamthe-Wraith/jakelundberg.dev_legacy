cc2c84006d2def28233da32e9540307e35e6e644
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-01 18:38:34 -0400

    re-removes firefox from ci workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,6 @@ const config: PlaywrightTestConfig = {
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
