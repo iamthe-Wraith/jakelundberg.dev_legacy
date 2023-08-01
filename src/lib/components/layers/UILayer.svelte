4676e1ae96fe891fad08998ec7f93f3fc99bb90b
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-01 14:33:42 -0400

    extracts ui layer into its own component

---
+++ src/lib/components/layers/UILayer.svelte
@@ -0,0 +1,17 @@
+<div class="layer ui-layer">
+  <slot />
+</div>
+
+<style lang="scss">
+  .ui-layer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    z-index: 10;
+  }
+</style>
