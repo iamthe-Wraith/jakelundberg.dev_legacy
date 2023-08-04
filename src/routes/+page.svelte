d5506789b28c8a8f1fb7b4276eb0bc3df0c0decb
Jake Lundberg <allhailthewraith@protonmail.com>
2023-08-04 16:10:57 -0400

    Issue/37 homepage intro (#59)
    * updates all fonts
    
    * updates the homepage intro text
    
    * updates homepage tests with new content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,9 @@
   <UILayer>
     <div class="ui-main">
       <div class="greeting">
-        <h1>Lorem ipsum <span>dolor sit amet</span>.</h1>
+        <h1><span>Hi, I'm</span>Jake Lundberg<span>.</span></h1>
+        <p class="subheader header-font">I build stuff for the web.</p>
+        <p class="intro-text">I'm a software engineer with an uncommon passion for what I do. There are few things that bring me as much joy as digging into complex problems to find elegant solutions, or brainstorming with smart people to find the idea that will make someone's life better.</p>
       </div>
     </div>
   </UILayer>
@@ -62,14 +64,53 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+    width: 100%;
+    height: 100%;
     align-items: center;
   }
 
   .greeting {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
     width: 90vw;
+    max-width: 45rem;
+    padding: 3rem 2rem 2rem;
+    border: 1px solid var(--dark-500);
+    border-radius: 0.75rem;
+    background-color: oklch(0% 0 0 / 0.75);
+
+    h1 {
+      position: relative;
+      color: var(--primary-500);
+
+      span {
+        color: var(--light-500);
+        font-weight: 400;
+        font-size: 2rem;
+        line-height: 1rem;
+
+        &:first-child {
+          position: absolute;
+          bottom: 100%;
+          left: 0;
+          font-size: 1.2rem;
+        }
+      }
+    }
+
+    .subheader {
+      margin: 1rem 0 1.5rem;
+      font-size: 1.3rem;
+      text-indent: 0;
+    }
 
     @media (min-width: 768px) {
+      align-items: center;
       width: 50vw;
+      background-color: oklch(0% 0 0 / 0.65);
     }
   }
 </style>
