3fb9e31ab89d40bce2169111842c461c8fb041d4
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-04 15:43:17 -0400

    updates the homepage intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,9 @@
   <UILayer>
     <div class="ui-main">
       <div class="greeting">
-        <h1>Lorem ipsum <span>dolor sit amet</span>.</h1>
+        <h1><span>Hi, I'm</span>Jake Lundberg<span>.</span></h1>
+        <p class="sub-header header-font">I build solutions on the web.</p>
+        <p>I'm a software engineer with an uncommon passion for what I do. There are few things that bring me as much joy as digging into complex problems to find elegant solutions, or brainstorming with smart people to find the idea that will make someone's life better.</p>
       </div>
     </div>
   </UILayer>
@@ -62,11 +64,45 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+    width: 100%;
+    height: 100%;
     align-items: center;
   }
 
   .greeting {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     width: 90vw;
+    max-width: 45rem;
+    padding: 4rem 2rem 2rem;
+    border-radius: 1rem;
+    background-color: oklch(0% 0 0 / 0.5);
+
+    h1 {
+      position: relative;
+      color: var(--primary-500);
+
+      span {
+        color: var(--light-500);
+        font-weight: 400;
+        font-size: 2rem;
+        line-height: 1rem;
+
+        &:first-child {
+          position: absolute;
+          bottom: 100%;
+          left: 0;
+          font-size: 1.2rem;
+        }
+      }
+    }
+
+    .sub-header {
+      margin: 1rem 0;
+      font-size: 1.5rem;
+    }
 
     @media (min-width: 768px) {
       width: 50vw;
