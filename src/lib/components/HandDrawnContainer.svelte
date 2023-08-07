8b74cbab1a7050e87034cc86c91a788552ea9ca2
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-07 11:50:46 -0400

    adds new HandDrawnContainer component

---
+++ src/lib/components/HandDrawnContainer.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { getRandom } from "$lib/utils/number";
+
+  export let hoverable = false;
+  export let hovered = false;
+  export let thickness = 3;
+  export let type: 'primary' | 'secondary' | 'neutral' = 'neutral';
+
+  const minorRadius = getRandom(13, 19);
+  const majorRadius = getRandom(230, 260);
+</script>
+
+<div
+  class="hand-drawn-container {hovered ? 'hovered' : ''} {hoverable ? 'hoverable' : ''} {type}"
+  on:mouseenter
+  on:mouseleave
+  style="
+    border-width: {thickness}px;
+    border-top-left-radius: {majorRadius}px {minorRadius}px;
+    border-top-right-radius: {minorRadius}px {majorRadius}px;
+    border-bottom-right-radius: {majorRadius}px {minorRadius}px;
+    border-bottom-left-radius: {minorRadius}px {majorRadius}px;
+  "
+>
+  <slot />
+</div>
+
+<style lang="scss">
+  .hand-drawn-container {
+    padding: 1rem 1.5rem;
+    background: oklch(0% 0 0 / 0.5);
+    border-style: solid;
+    transition: border-color 0.25s ease-in-out;
+
+    @keyframes wiggle {
+      0% {
+        transform: rotate(-0.6deg);
+      }
+
+      50% {
+        transform: rotate(0.6deg);
+      }
+
+      100% {
+        transform: rotate(-0.6deg);
+      }
+    }
+
+    &.hoverable {
+      &.primary.hovered,
+      &.secondary.hovered,
+      &.primary:hover,
+      &.secondary:hover {
+        border-color: var(--dark-500);
+        animation: wiggle 0.5s ease-in-out infinite;
+      }
+
+      &.neutral.hovered,
+      &.neutral:hover {
+        border-color: var(--secondary-500);
+        animation: wiggle 0.5s ease-in-out infinite;
+      }
+    }
+
+    &.primary {
+      border-color: var(--primary-500);
+    }
+
+    &.secondary {
+      border-color: var(--secondary-500);
+    }
+
+    &.neutral {
+      border-color: var(--dark-500);
+    }
+  }
+</style>
