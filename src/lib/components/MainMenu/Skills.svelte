aa9a710e06972acef370b8dc846c9183e1abef08
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-10 10:56:26 -0400

    adds init list of skills

---
+++ src/lib/components/MainMenu/Skills.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { ISkill } from "$lib/types/skills";
+  import { getContext } from "svelte";
+
+  const skills = getContext<ISkill[]>('skills');
+</script>
+
+<div class="skills">
+  {#each (skills || []) as skill}
+    <p>{skill.name}</p>
+  {/each}
+</div>
