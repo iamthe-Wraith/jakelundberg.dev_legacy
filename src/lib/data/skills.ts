f785bc9c666a9c49f3e0ee1b8c9ef8c4551996b7
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-10 10:22:43 -0400

    adds init skills data files

---
+++ src/lib/data/skills.ts
@@ -0,0 +1,73 @@
+import type { ISkill } from '$lib/types/skills';
+
+export const codingSkills: ISkill[] = [
+  {
+    name: 'Blender',
+  },
+  {
+    name: 'CSS',
+  },
+  {
+    name: 'Electron',
+  },
+  {
+    name: 'ExpressJS',
+  },
+  {
+    name: 'Figma',
+  },
+  {
+    name: 'Git',
+  },
+  {
+    name: 'GitHub',
+  },
+  {
+    name: 'GitHub Copilot',
+  },
+  {
+    name: 'HTML',
+  },
+  {
+    name: 'JavaScript',
+  },
+  {
+    name: 'MobX',
+  },
+  {
+    name: 'MongoDB',
+  },
+  {
+    name: 'Node',
+  },
+  {
+    name: 'PostgreSQL',
+  },
+  {
+    name: 'React',
+  },
+  {
+    name: 'React Native',
+  },
+  {
+    name: 'Redis',
+  },
+  {
+    name: 'Redux',
+  },
+  {
+    name: 'Svelte/SvelteKit',
+  },
+  {
+    name: 'ThreeJS',
+  },
+  {
+    name: 'TypeScript',
+  },
+  {
+    name: 'Vite',
+  },
+  {
+    name: 'Webpack',
+  },
+];
