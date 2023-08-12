a85b5f07f62d8832855bb2c261e2efa00a5d7852
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-11 22:41:58 -0400

    adds logos to all skills

--- src/lib/components/MainMenu/Skills.svelte
+++ src/lib/components/MainMenu/Skills.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { assets } from "$app/paths";
   import type { ISkill } from "$lib/types/skills";
   import { getContext } from "svelte";
 
@@ -8,7 +9,59 @@
 <div class="skills">
   {#each (skills || []) as skill}
     <div class="skill">
-      <p class="skill-name">{skill.name}</p>
+      <div class="skill-logo">
+        <img src={`${assets}/skills/${skill.image}`} alt={`${skill.name} logo`} />
+      </div>
+      <div class="skill-details">
+        <p class="skill-name">{skill.name}</p>
+      </div>
     </div>
   {/each}
 </div>
+
+<style lang="scss">
+  .skills {
+    padding: 0 0 2rem;
+    column-count: 1;
+
+    @media (min-width: 768px) {
+      padding: 0 1rem 2rem;
+      column-count: 2;
+    }
+  }
+
+  .skill {
+    display: flex;
+
+    &:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+
+    .skill-logo {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 3rem;
+      height: 3rem;
+      margin-right: 1rem;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+      }
+
+      @media (min-width: 768px) {
+        width: 4rem;
+        height: 4rem;
+      }
+    }
+
+    .skill-details {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      flex: 1;
+    }
+  }
+</style>
--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -3,71 +3,117 @@ import type { ISkill } from '$lib/types/skills';
 export const skills: ISkill[] = [
   {
     name: 'Blender',
+    image: 'blender.png',
+    comfortLevel: 1,
   },
   {
     name: 'CSS',
+    image: 'css.png',
+    comfortLevel: 4,
   },
   {
     name: 'Electron',
+    image: 'electron.png',
+    comfortLevel: 2,
   },
   {
     name: 'ExpressJS',
+    image: 'express.png',
+    comfortLevel: 3,
   },
   {
     name: 'Figma',
+    image: 'figma.png',
+    comfortLevel: 2,
   },
   {
     name: 'Git',
+    image: 'git.png',
+    comfortLevel: 3,
   },
   {
     name: 'GitHub',
+    image: 'github.png',
+    comfortLevel: 4,
   },
   {
     name: 'GitHub Copilot',
+    image: 'github-copilot.png',
+    comfortLevel: 4,
   },
   {
     name: 'HTML',
+    image: 'html.png',
+    comfortLevel: 4,
   },
   {
     name: 'JavaScript',
+    image: 'javascript.png',
+    comfortLevel: 5,
   },
   {
     name: 'MobX',
+    image: 'mobx.png',
+    comfortLevel: 3,
   },
   {
     name: 'MongoDB',
+    image: 'mongodb.png',
+    comfortLevel: 3,
   },
   {
     name: 'Node',
+    image: 'node.png',
+    comfortLevel: 4,
   },
   {
     name: 'PostgreSQL',
+    image: 'postgres.png',
+    comfortLevel: 3,
   },
   {
     name: 'React',
+    image: 'react.png',
+    comfortLevel: 4,
   },
   {
     name: 'React Native',
+    image: 'react-native.png',
+    comfortLevel: 2,
   },
   {
     name: 'Redis',
+    image: 'redis.png',
+    comfortLevel: 3,
   },
   {
     name: 'Redux',
+    image: 'redux.png',
+    comfortLevel: 3,
   },
   {
     name: 'Svelte/SvelteKit',
+    image: 'svelte.png',
+    comfortLevel: 4,
   },
   {
     name: 'ThreeJS',
+    image: 'three-js.png',
+    comfortLevel: 3,
   },
   {
     name: 'TypeScript',
+    image: 'typescript.png',
+    comfortLevel: 4,
   },
   {
     name: 'Vite',
+    image: 'vite.png',
+    comfortLevel: 3,
   },
   {
     name: 'Webpack',
+    image: 'webpack.png',
+    comfortLevel: 3,
   },
 ];
