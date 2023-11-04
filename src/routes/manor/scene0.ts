c7c500f16b895340c3d152e53bed0d9528b2f002
Jake Lundberg <allhailthewraith@protonmail.com>
2023-11-04 09:43:33 -0400

    Issue/79 cemetery gate brazier sparks (#100)

--- src/routes/manor/scene0.ts
+++ src/routes/manor/scene0.ts
@@ -1,7 +1,22 @@
-import { quaternary700HexColor, tertiary100HexColor } from '$lib/constants/colors';
+import {
+	quaternary400HexColor,
+	quaternary600HexColor,
+	quaternary700HexColor,
+	quaternary800HexColor,
+	tertiary100HexColor
+} from '$lib/constants/colors';
 import type { AssetNames } from '$lib/stores/assets';
+import { getRandomNum } from '$lib/utils/number';
 import { WraithScene } from '$lib/utils/scene';
 import * as THREE from 'three';
+
+interface ISpark {
+	parent: THREE.Mesh;
+	mesh: THREE.Mesh;
+	speed: number;
+	duration: number;
+	lifespan: number;
+}
 
 export class ManorScene0 extends WraithScene {
 	public name = 'manor-s0-anchor';
@@ -12,6 +27,16 @@ export class ManorScene0 extends WraithScene {
 
 	private gate1: THREE.Mesh | null = null;
 	private gate2: THREE.Mesh | null = null;
+
+	// #region sparks
+	private readonly sparksCount = 16;
+	private readonly sparkColors = [
+		quaternary400HexColor,
+		quaternary600HexColor,
+		quaternary800HexColor
+	];
+	private sparks: ISpark[] = [];
+	// #endregion
 
 	constructor(assets: Record<AssetNames, THREE.Mesh | THREE.Group | null>) {
 		super();
@@ -213,11 +238,15 @@ export class ManorScene0 extends WraithScene {
 			brazier1_1.rotateY(THREE.MathUtils.degToRad(19));
 			this.anchor.add(brazier1_1);
 
+			this.initSparks(brazier1_1);
+
 			const brazier1_2 = assets['brazier-1'].clone() as THREE.Mesh;
 			brazier1_2.name = 'manor-s0-brazier1_2';
 			brazier1_2.position.set(1.85, -1, 2.8);
 			brazier1_2.rotateY(THREE.MathUtils.degToRad(124));
 			this.anchor.add(brazier1_2);
+
+			this.initSparks(brazier1_2);
 
 			const brazierLight1 = new THREE.PointLight(quaternary700HexColor, 5, 40);
 			brazierLight1.name = 'manor-0-brazierLight1';
@@ -251,19 +280,12 @@ export class ManorScene0 extends WraithScene {
 	) => {
 		if (!this.isLoaded()) return;
 
-		// const delta = clock.getDelta();
-
-		// console.log('rotation', camera.rotation.x, camera.rotation.y, camera.rotation.z);
-
-		// this.addToScene(scene);
-
 		if (inView) {
 			this.animateIntoView(scene);
+			this.animateSparks(clock);
 		} else {
 			this.animateOutOfView(scene);
 		}
-
-		// this.animateOutOfView(scene);
 
 		// if (camera.position.z > 2.25) {
 		//   if (this.gate1 && THREE.MathUtils.radToDeg(this.gate1.rotation.y) > -90) {
@@ -279,5 +301,84 @@ export class ManorScene0 extends WraithScene {
 	protected isLoaded = () => {
 		this.loaded = this.loaded || !!this.anchor.getObjectByName('manor-s0-tree4_1');
 		return this.loaded;
+	};
+
+	private animateSparks = (clock: THREE.Clock) => {
+		const delta = clock.getDelta();
+
+		for (let i = 0; i < this.sparks.length; i++) {
+			let spark = this.sparks[i];
+
+			if (spark.duration > spark.lifespan) {
+				spark.parent.remove(spark.mesh);
+				this.sparks.splice(i, 1);
+				spark = this.initSpark(spark.parent);
+				this.sparks.push(spark);
+			}
+
+			spark.duration += delta;
+			(spark.mesh.material as THREE.Material).opacity = 1 - spark.duration / spark.lifespan;
+
+			const scaleDegradation = 1 - delta;
+			spark.mesh.geometry.scale(scaleDegradation, scaleDegradation, scaleDegradation);
+
+			spark.mesh.position.set(
+				spark.mesh.position.x + (spark.duration / spark.lifespan) * delta,
+				spark.mesh.position.y + spark.speed * delta,
+				spark.mesh.position.z + (spark.duration / spark.lifespan) * delta
+			);
+		}
+	};
+
+	private initSpark = (
+		base: THREE.Mesh<
+			THREE.BufferGeometry<THREE.NormalBufferAttributes>,
+			THREE.Material | THREE.Material[]
+		>
+	): ISpark => {
+		const color = this.sparkColors[Math.round(getRandomNum() * (this.sparkColors.length - 1))];
+
+		const mesh = new THREE.Mesh(
+			new THREE.BoxGeometry(0.5, 0.5, 0.5),
+			new THREE.MeshStandardMaterial({
+				color,
+				emissive: color,
+				emissiveIntensity: 5,
+				transparent: true,
+				opacity: getRandomNum()
+			})
+		);
+
+		base.add(mesh);
+
+		mesh.position.set(
+			getRandomNum() * 0.55 * (getRandomNum() > 0.5 ? 1 : -1),
+			1.1,
+			getRandomNum() * 0.55 * (getRandomNum() > 0.5 ? 1 : -1)
+		);
+
+		mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 7.5);
+
+		const scale = getRandomNum() * 0.5;
+		mesh.geometry.scale(scale, scale, scale);
+
+		return {
+			parent: base,
+			mesh,
+			speed: 3 * getRandomNum() + 0.8,
+			duration: 0,
+			lifespan: getRandomNum() * 0.7
+		};
+	};
+
+	private initSparks = (
+		base: THREE.Mesh<
+			THREE.BufferGeometry<THREE.NormalBufferAttributes>,
+			THREE.Material | THREE.Material[]
+		>
+	) => {
+		for (let i = 0; i < this.sparksCount; i++) {
+			this.sparks.push(this.initSpark(base));
+		}
 	};
 }
